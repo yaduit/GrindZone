f41b929dfde69f02e148304c6597a13622ca20bb
yaduit <yk226411@gmail.com>
2025-08-19 13:31:02 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Generator from "./components/Generator"
+import Hero from "./components/Hero"
+import Workout from "./components/Workout"
+
+function App() {
+  return(<main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
+  <Hero/>
+  <Generator/>
+  <Workout/>
+
+  
+  
+  </main>)
+}
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto'>
+    <div className='flex flex-col gap-5'>
+      <h1 className="uppercase text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold tracking-tight
+            bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500
+            bg-clip-text text-transparent">grind zone</h1>
+        <h3 className='font-semibold text-1xl sm:text-2xl md:text-3xl lg:text-4xl'>Transform Your <span className='text-blue-400 font-medium'>Body</span> Elevate Your <span className='text-blue-400 font-medium'>Mind</span></h3>
+        </div>
+        <p className='text-sm md:text-base font-light py-3 sm:text-lg leading leading-relaxed'>Your journey to strength, confidence, and discipline starts here.Track your progress, follow expert routines, and stay motivated—because your best self is waiting to be unlocked.</p>
+        <button className='px-8 py-4 rounded-md  border-[2px] border-blue-400 border-solid bg-slate-950 blueShadow'> 
+          <p>Accept & begin</p>
+        </button>
+
+      
+    </div>
+  )
+}
