0b21129501fbb384bdfe58d4bc447412e105b335
yaduit <yk226411@gmail.com>
2025-08-24 12:56:07 +0530

    section generator first part

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import SectionWrapper from './sectionWrapper'
+
+
+function Header(props){
+  const{index,title,description} = props
+
+  return(
+    <div className='flex flex-col gap-4'>
+      <div className='flex items-center gap-2'>
+        <p className='text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400'>{index}</p>
+        <h4 className='text-lg sm:text-2xl md:text-3xl'>{title}</h4>
+      </div>
+      <p>{description}</p>
+    </div>
+  )
+}
+
+
 
 export default function Generator() {
   return (
-    <div>Generator</div>
+    <SectionWrapper header={"generate your workout"} title={['it\'s','huge','o\'clock']}>
+
+      <Header index={'01'} title={'pick your poison'} description={'select the workout you wish to endure'}/>
+       
+    </SectionWrapper>
   )
 }
